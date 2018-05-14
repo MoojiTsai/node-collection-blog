2a51db366c7bda9a1c60998ca0d94967fce7e2f6
WoodyTsai <starts789@gmail.com>
2018-05-14 09:09:30 +0800

    changescript to production

--- config/default.js
+++ config/default.js
@@ -25,6 +25,11 @@ module.exports = function(){
               key: 'node-collection',
               maxAge: 2592000000
             },
+            cloudary:{
+              CLOUD_NAME:'hqa0ftnmu', 
+              CLOUD_KEY:'724457381865382',
+              CLOUD_SECRET:'VGNcz4VNRN8yKqJUltbHf_ZD_q4'
+            },
             mongodb: 'mongodb://mooji:starts999@ds117590.mlab.com:17590/node-collection'
           };
 
