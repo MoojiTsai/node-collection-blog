483b346b43252ad9c3fceec522ac9d69ddedebe7
WoodyTsai <starts789@gmail.com>
2018-05-16 21:59:35 +0800

    add || port 5000

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ app.use(function (err, req, res, next) {
 });
 
 // 监听端口，启动程序
-app.listen(config.port, function () {
+app.listen(config.port|| 5000, function () {
   console.log(`${pkg.name} listening on port ${config.port}`)
 })
 
